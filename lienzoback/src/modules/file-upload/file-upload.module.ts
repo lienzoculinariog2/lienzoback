ca22eaefc0881c08ac96becc12a5aba7ff23c7f4
CMolinaBetancourt <cathemolinab@gmail.com>
2025-08-08 08:06:11 -0700

    feature/categories/create/upload with imgURL

--- lienzoback/src/modules/file-upload/file-upload.module.ts
+++ lienzoback/src/modules/file-upload/file-upload.module.ts
@@ -25,5 +25,6 @@ export const CLOUDINARY = 'Cloudinary';
       inject: [ConfigService],
     },
   ],
+  exports: [FileUploadService],
 })
 export class FileUploadModule {}
--- lienzoback/src/modules/products/products.controller.ts
+++ lienzoback/src/modules/products/products.controller.ts
@@ -33,6 +33,11 @@ export class ProductsController {
     return this.productsService.getProductById(id);
   }
 
+  @Put(':id')
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(id, updateProductDto);
+  }
+
   @Put(':id/image')
   updateImage(@Param('id') id: string, @Body('imgUrl') imgUrl: string) {
     return this.productsService.updateProductImage(id, imgUrl);
